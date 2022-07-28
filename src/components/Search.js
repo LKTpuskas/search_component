e6c0d7a360dd468822afce354077be57cc5582fb
Lorenz Puskas <lorenzpuskas@hotmail.com>
2022-07-28 11:05:30 +0100

    resolved react-hooks/exhaustive deps. Added useMemo and useCallback

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import debounce from "lodash/debounce";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { useDispatch } from "react-redux";
 
 import OutsideClick from "../OutsideClick";
@@ -14,35 +14,31 @@ function Search() {
   const [searches, setSearches] = useState({});
   const [query, setQuery] = useState("");
 
-  /* eslint-disable react-hooks/exhaustive-deps */
-  const searchRequesHandler = useCallback(
-    debounce((searchTerm) => {
-      setQuery(searchTerm);
-      search(searchTerm);
-    }, 400),
-    []
-  );
-  /* eslint-disable react-hooks/exhaustive-deps */
-
-  function search(query) {
-    function searchSuccessHandler(data) {
-      if (data?.length > 0) {
-        dispatch(setSearchResults(data));
-      } else {
-        dispatch(setSearchResults([]));
+  const search = useCallback(
+    (query) => {
+      setQuery(query);
+      function searchSuccessHandler(data) {
+        if (data?.length > 0) {
+          dispatch(setSearchResults(data));
+        } else {
+          dispatch(setSearchResults([]));
+        }
       }
-    }
 
-    // Don't make the same search twice
-    if (query && getStoredSearch(query, searches)) {
-      searchSuccessHandler(getStoredSearch(query, searches));
-    } else if (query.length > 0) {
-      searchRequest(query).then((data) => {
-        searchSuccessHandler(data);
-        setSearches(Object.assign(searches, { [query.toLowerCase()]: data }));
-      });
-    }
-  }
+      // Don't make the same search twice
+      if (query && getStoredSearch(query, searches)) {
+        searchSuccessHandler(getStoredSearch(query, searches));
+      } else if (query?.length > 0) {
+        searchRequest(query).then((data) => {
+          searchSuccessHandler(data);
+          setSearches(Object.assign(searches, { [query.toLowerCase()]: data }));
+        });
+      }
+    },
+    [dispatch, searches]
+  );
+
+  const searchRequesHandler = useMemo(() => debounce(search, 400), [search]);
 
   return (
     <OutsideClick
@@ -56,7 +52,7 @@ function Search() {
           onClick={() => setSearchOpen(true)}
           onChange={({ target: { value } }) => searchRequesHandler(value)}
         />
-        <Dropdown searchOpen={query.length > 0 && searchOpen} />
+        <Dropdown searchOpen={query?.length > 0 && searchOpen} />
       </StyledSearchForm>
     </OutsideClick>
   );
